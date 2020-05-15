f80ccc68eba618b7c3fdade17ca81211672c3ba3
Colin O'Keeffe <colin@basecamp.com>
2020-05-15 23:01:46 +0200

    Revert "Pass in the job as an argument of the callback"
    This reverts commit 7b18534ac564f9f18a9e40f5947a6a2e7491bcb2.

--- lib/active_job/stats/callbacks.rb
+++ lib/active_job/stats/callbacks.rb
@@ -2,42 +2,42 @@ module ActiveJob
   module Stats
     module Callbacks
       extend ActiveSupport::Concern
-
       included do
-        before_enqueue ->(job) { after_enqueue_stats(job) }, if: :monitored
-        after_enqueue  ->(job) { after_enqueue_stats(job) },  if: :monitored
-        before_perform ->(job) { before_perform_stats(job) }, if: :monitored
-        after_perform  ->(job) { after_perform_stats(job) },  if: :monitored
-        around_perform ->(job, block) { benchmark_stats(job, block) },      if: :benchmarked
+        before_enqueue :after_enqueue_stats,  if: :monitored
+        after_enqueue  :after_enqueue_stats,  if: :monitored
+        before_perform :before_perform_stats, if: :monitored
+        after_perform  :after_perform_stats,  if: :monitored
+
+        around_perform :benchmark_stats,      if: :benchmarked
 
         private
 
-        def benchmark_stats(job, block)
+        def benchmark_stats
           require 'active_support/core_ext/benchmark'
-          benchmark = Benchmark.ms { block.call }
-          ActiveJob::Stats.reporter.timing("#{job.queue_name}.processed", benchmark)
-          ActiveJob::Stats.reporter.timing("#{job.class}.processed", benchmark)
-          ActiveJob::Stats.reporter.timing("#{job.class}.#{ENV['RAILS_ENV']}.processed", benchmark)
+          benchmark = Benchmark.ms { yield }
+          ActiveJob::Stats.reporter.timing("#{self.class.queue_name}.processed", benchmark)
+          ActiveJob::Stats.reporter.timing("#{self.class}.processed", benchmark)
+          ActiveJob::Stats.reporter.timing("#{self.class}.#{ENV['RAILS_ENV']}.processed", benchmark)
         end
 
-        def before_perform_stats(job)
-          ActiveJob::Stats.reporter.increment("#{job.queue_name}.started")
-          ActiveJob::Stats.reporter.increment("#{job.class}.started")
-          ActiveJob::Stats.reporter.increment("#{job.class}.#{ENV['RAILS_ENV']}.started")
+        def before_perform_stats
+          ActiveJob::Stats.reporter.increment("#{self.class.queue_name}.started")
+          ActiveJob::Stats.reporter.increment("#{self.class}.started")
+          ActiveJob::Stats.reporter.increment("#{self.class}.#{ENV['RAILS_ENV']}.started")
           ActiveJob::Stats.reporter.increment('total.started')
         end
 
-        def after_enqueue_stats(job)
-          ActiveJob::Stats.reporter.increment("#{job.queue_name}.enqueued")
-          ActiveJob::Stats.reporter.increment("#{job.class}.enqueued")
-          ActiveJob::Stats.reporter.increment("#{job.class}.#{ENV['RAILS_ENV']}.enqueued")
+        def after_enqueue_stats
+          ActiveJob::Stats.reporter.increment("#{self.class.queue_name}.enqueued")
+          ActiveJob::Stats.reporter.increment("#{self.class}.enqueued")
+          ActiveJob::Stats.reporter.increment("#{self.class}.#{ENV['RAILS_ENV']}.enqueued")
           ActiveJob::Stats.reporter.increment('total.enqueued')
         end
 
-        def after_perform_stats(job)
-          ActiveJob::Stats.reporter.increment("#{job.queue_name}.finished")
-          ActiveJob::Stats.reporter.increment("#{job.class}.finished")
-          ActiveJob::Stats.reporter.increment("#{job.class}.#{ENV['RAILS_ENV']}.finished")
+        def after_perform_stats
+          ActiveJob::Stats.reporter.increment("#{self.class.queue_name}.finished")
+          ActiveJob::Stats.reporter.increment("#{self.class}.finished")
+          ActiveJob::Stats.reporter.increment("#{self.class}.#{ENV['RAILS_ENV']}.finished")
           ActiveJob::Stats.reporter.increment('total.finished')
         end
 
